5219c8b4b20284c75f723afadbfd11d3cc0fd524
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-02-19 21:58:49 -0500

    Updating pages for specific categories

--- react-ecommerce-example/src/components/NavBar.js
+++ react-ecommerce-example/src/components/NavBar.js
@@ -2,8 +2,14 @@ export const NavBar = () => {
     return (
         <nav className="grid-columns-three">
             <a href="/">Home</a>
+            <a href="/electronics">Electronics</a>
+            <a href="/selfcare">Self-Care</a>
+            <a href="/groceries">Groceries</a>
+            <a href="/homegoods">Home Goods</a>
+            <a href="/clothes">Clothes</a>
+            <a href="/automotives">Automotives</a>
             <a href="/">Search</a>
-            <a href="/">Account</a>
+            <a href="/account">Account</a>
         </nav>
     );
 };
